f57a14e8f81e31886165ec6478172f6f0d02a9ab
edupoch <edupoch@gmail.com>
2025-06-27 17:24:16 +0200

    Mejora la consistencia del formato en los archivos de configuración y HTML

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,10 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import type { Route } from "./+types/root";
 import "./app.css";
