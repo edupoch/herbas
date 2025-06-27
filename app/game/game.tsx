2d2c5c00b845ae10e332b5478eaa53c515bd8907
edupoch <edupoch@gmail.com>
2025-06-27 13:20:36 +0200

    Loop básico

--- app/game/game.tsx
+++ app/game/game.tsx
@@ -4,20 +4,76 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 export function Game() {
-    const [counter, setCounter] = useState(0);
+    const posiblesHerbas = [
+        'Fiuncho',
+        'Malva',
+        'Romeu',
+        'Rosal silvestre',
+        'Herba de San Xoán',
+        'Herba luisa',
+        'Fento macho',
+        'Sabugueiro',
+        'Flor de San Xoán',
+        'Codeso',
+        'Nogueira',
+        'Loureiro',
+        'Torvisco',
+        'Ruda',
+        'Hierbabuena',
+        'Salvia',
+        'Artemisa'
+    ];
+
+    const nElementos = 9;
+    
+    const [inventario, setInventario] = useState(Array(nElementos).fill(null));
+    const [ano, setAno] = useState(1);
+    
+    const collerHerbas = function() {
+        setInventario(prevInventario => {
+            const novoInventario = [...prevInventario];
+            for (let i = 0; i < novoInventario.length; i++) {
+                novoInventario[i] = posiblesHerbas[Math.floor(Math.random() * posiblesHerbas.length)];
+            }
+            return novoInventario;
+        });
+    };
+
+    const facerCacho = function() {
+        setInventario(Array(nElementos).fill(null));
+        setAno(ano + 1);
+    };
+
+    const listaInventario = inventario.map((item, index) => (
+        <Grid size={4} key={index}>
+            <Paper elevation={3} style={{ padding: '16px' }}>
+                <Typography variant="body1">
+                    {item ? `${item}` : `Nada`}
+                </Typography>
+            </Paper>
+        </Grid>
+    ));
 
     return (
         <Container maxWidth="sm">
-            <Button variant="contained" onClick={() => setCounter(counter + 1)}>
-                Click me
+            <Typography variant="h4" component="h1" gutterBottom>
+                
+            Ano {ano}
+            </Typography>
+            <Button variant="contained" onClick={collerHerbas}>
+                Ir coller herbas
             </Button>
-            <Paper elevation={3} style={{ padding: '16px', marginTop: '16px', fontFamily: 'body1' }}>
-                <Typography>
-                    <p>You clicked {counter} times</p>
-                </Typography>
-            </Paper>
+
+            <Grid style={{marginTop: '16px', marginBottom: '16px'}} container rowSpacing={2} columnSpacing={2}>
+                {listaInventario}
+            </Grid>
+
+            <Button variant="contained" onClick={facerCacho}>
+                Facer o cacho
+            </Button>
         </Container>
     );
 }
